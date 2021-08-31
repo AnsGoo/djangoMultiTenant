61978f0681c615eb5c546531a074c7c7fe25fe59
ranger <haiven_123@163.com>
2021-08-31 22:05:29 +0800

    feat(): 完成了自动建表功能

--- mult_tenant/tenant/signal.py
+++ mult_tenant/tenant/signal.py
@@ -1,8 +1,10 @@
+from threading import Thread
 from mult_tenant.tenant.models import Tenant
 from django.db.models.signals import post_save   # 另外一个内置的常用信号
 import logging
 from django.dispatch import receiver
 from mult_tenant.tenant.utils.model import get_tenant_model
+
 
 Tenant = get_tenant_model()
 logger = logging.getLogger('django.request')
@@ -13,3 +15,21 @@ def create_data_handler(sender, signal, instance, created:bool, **kwargs):
     if created:
         instance.create_database()
         logger.info(f'create database : [{instance.db_name}] successfuly for {instance.code}')
+
+
+        thread = Thread(target=migrate,args=[instance.code])
+        thread.start()
+        
+
+def migrate(database):
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        logger.error('migrate fail')
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(['manage.py', 'multmigrate', f'--database={database}'])
+    logger.error('migrate successfuly, create_table successfuly')
